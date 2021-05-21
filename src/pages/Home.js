8ec9914d48f9edbae81682513d07f71bffbbe031
Emil Schultz <emilrschultz@gmail.com>
2021-05-21 17:45:39 +0200

    chore: header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import Header from "../components/Header";
+import Avatar from "../components/Avatar";
+import Button from "../components/Button";
 
 export default function Home() {
   return (
     <section>
-      <Header left={<p>left</p>} right={<p>right</p>}></Header>
+      <Header
+        left={<Avatar src="https://i.pravatar.cc/300">Hi Fradde</Avatar>}
+        right={
+          <Button variant="transparent">
+            <i className="bi bi-search"></i>
+          </Button>
+        }
+      ></Header>
     </section>
   );
 }
