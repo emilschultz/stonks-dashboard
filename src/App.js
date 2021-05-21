a78afbad0cdac4793b1a9020c40062d3bbaa5eb2
Emil Schultz <emilrschultz@gmail.com>
2021-05-21 17:34:40 +0200

    feat: Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import "./styles/base.css";
+import "./styles/variables.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div>
-      <p> hey</p>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
