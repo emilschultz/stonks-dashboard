56d1a6399156ad57d5052dd13b4a0e5894e4eb71
Emil Schultz <emilrschultz@gmail.com>
2021-05-21 15:30:06 +0200

    style: Header component init

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "../components/header.css";
+
+export default function Header({ left = null, right = null }) {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__left">{left}</div>
+        <div className="header__left">{right}</div>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <section>
+      <Header left={<p>left</p>} right={<p>right</p>}></Header>
+    </section>
+  );
+}
