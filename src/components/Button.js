c0ec2c64398a8c773b3f2559f24eaedc4ecca62c
Emil Schultz <emilrschultz@gmail.com>
2021-05-23 15:22:57 +0200

    Added Sign out button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import "./Button.css";
 import cn from "../utils/classnames";
+import app from "../config/firebase";
 
 export default function Button({
   children,
@@ -16,13 +17,16 @@ export default function Button({
     [`button--${variant}`]: variant,
   });
   return (
-    <button
-      onMouseLeave={onMouseLeave}
-      onMouseOver={onMouseOver}
-      onClick={onClick}
-      className={classNames}
-    >
-      {children}
-    </button>
+    <>
+      <button
+        onMouseLeave={onMouseLeave}
+        onMouseOver={onMouseOver}
+        onClick={onClick}
+        className={classNames}
+      >
+        {children}
+      </button>
+      <button onClick={() => app.auth().signOut()}>Sign out</button>
+    </>
   );
 }
