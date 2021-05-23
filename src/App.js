901fe21924bb1ce031b690e6d1265b5c5910dda1
Emil Schultz <emilrschultz@gmail.com>
2021-05-23 15:22:30 +0200

    chore: Routing changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import "./styles/base.css";
 import "./styles/variables.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./config/Auth";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <AuthProvider>
+      <div>
+        <Router>
+          <Switch>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/SignUp" component={SignUp} />
+          </Switch>
+        </Router>
+      </div>
+    </AuthProvider>
   );
 }
 
