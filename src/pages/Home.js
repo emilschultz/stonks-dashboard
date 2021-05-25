23725d4f2a11d9365549635bbcd32652673f86bb
Emil Schultz <emilrschultz@gmail.com>
2021-05-25 15:12:42 +0200

    feat: New section - Today's trending stock

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,37 @@
 import Header from "../components/Header";
 import Avatar from "../components/Avatar";
 import Button from "../components/Button";
+import Section from "../components/Section";
+import Slider from "../components/Slider";
+import RandomStock from "../components/RandomStock";
+
+import { useContext } from "react";
+
+import { AuthContext } from "../config/Auth";
 
 export default function Home() {
+  const { currentUser } = useContext(AuthContext);
+
   return (
-    <section>
+    <main>
       <Header
-        left={<Avatar src="https://i.pravatar.cc/300">Hi Fradde</Avatar>}
+        left={
+          <Avatar src="https://i.pravatar.cc/300">
+            Hi {currentUser.email}
+          </Avatar>
+        }
         right={
           <Button variant="transparent">
             <i className="bi bi-search"></i>
           </Button>
         }
       ></Header>
-    </section>
+      <Section title="Today's trending stonk">
+        <RandomStock />
+      </Section>
+      <Section title="Trending stonks">
+        <Slider />
+      </Section>
+    </main>
   );
 }
